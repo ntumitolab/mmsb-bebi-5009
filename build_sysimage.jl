3be827040a726560a16ac274f5fcab253350e73d
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-03-22 21:36:21 +0800

    fix CI

---
+++ build_sysimage.jl
@@ -0,0 +1,12 @@
+import Pkg
+Pkg.add(["PackageCompiler", "IJulia"])
+using PackageCompiler
+PackageCompiler.create_sysimage(
+    ["Plots", "DifferentialEquations", "ModelingToolkit", "Catalyst"];
+    project=".", sysimage_path="sysimage.so")
+
+using IJulia
+IJulia.installkernel("Julia", "--sysimage=sysimage.so")
+
+Pkg.remove("PackageCompiler")
+Pkg.gc()
