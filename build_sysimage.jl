645b333619d750a5e78ea2295015794772fcd0ca
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-03-22 22:38:52 +0800

    fix sysimage path

--- build_sysimage.jl
+++ build_sysimage.jl
@@ -3,10 +3,10 @@ Pkg.add(["PackageCompiler", "IJulia"])
 using PackageCompiler
 PackageCompiler.create_sysimage(
     ["Plots", "DifferentialEquations", "ModelingToolkit", "Catalyst"];
-    project=".", sysimage_path="sysimage.so")
+    project=".", sysimage_path="$(pwd())/sysimage.so")
 
 using IJulia
-IJulia.installkernel("Julia", "--sysimage=sysimage.so")
+IJulia.installkernel("Julia", "--sysimage=$(pwd())/sysimage.so")
 
 Pkg.rm("PackageCompiler")
 Pkg.gc()
