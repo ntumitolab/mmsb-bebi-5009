c93fb8522ef20208c1d273d5e8970fb1ddf7baa9
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-03-20 17:22:02 +0800

    [skip ci] ch5 model code

--- test.jl
+++ test.jl
@@ -1,37 +1,15 @@
-using DifferentialEquations
-using ModelingToolkit
-using Catalyst
-using Plots
-using LabelledArrays
-
-rn41 = @reaction_network begin
-    (hillr(B, k1, 1, n), k3), 0 <--> A
-    k5, A --> B
-    (k2, k4), B <--> 0
-end
-
-fieldnames(typeof(rn41))
-
-ps1 = [:k1=>20., :k2=>5., :k3=>5., :k4=>5., :k5=>2., :n=>4.]
-ps1_slv = let
-    d = Dict(ps1...)
-    nt = (;d...)
-    ps1_slv = SLVector(nt)
-end
-
-osys41 = convert(ODESystem, rn41)
-f41 = ODEFunction{false}(osys41)
-
-f41(SLVector(A=0, B=0), ps1_slv, 0.0)
-
 # methionine model
 
-using ComponentArrays
+using LabelledArrays
 using Plots
 using DifferentialEquations
 using UnPack
 
-ps = ComponentArray(
+# Convenience functions
+hil(x, k) = x / (x + k)
+hil(x, k, n) = hil(x^n, k^n)
+
+ps = (
     v_MATI_max=561., K_MATI_m=41., K_MATI_i=50.,
     v_MATIII_max=22870., K_MATIII_m2=21.1,
     v_MET_max=4544., A_over_K_MET_m2 = 0.1,
@@ -39,12 +17,21 @@ ps = ComponentArray(
     alpha_d=1333., K_AHC=0.1, Adenosine=1., Met=48.5
 )
 
-function metmodel!(D, u, p, t)
+function metmodel(u, p, t)
     @unpack AdoMet, AdoHcy = u
     K_MATIII_m1 = 20000 / (1 + 5.7 * (AdoMet / (AdoMet + 600)^2))
     K_MET_m1 = 10 * (1 + AdoHcy / 4)
     @unpack K_AHC, Adenosine = p
     Hcy = AdoHcy * K_AHC / Adenosine
+    @unpack v_MATI_max, Met, K_MATI_m, K_MATI_i = p
+    v_MATI = v_MATI_max * hil(Met, K_MATI_m) * hil(K_MATI_i, AdoMet)
+    @unpack v_MATIII_max, Met, K_MATIII_m2 = p
+    v_MATIII = v_MATIII_max * hil(Met, K_MATIII_m1 * hil(K_MATIII_m2, Met))
+    @unpack v_GNMT_max, K_GNMT_m, K_GNMT_i = p
+    v_GNMT = v_GNMT_max * hil(AdoMet, K_GNMT_m, 2.3) * hil(K_GNMT_i, AdoHcy)
+end
 
-
+function metmodel!(D, u, p, t)
+    D.AdoMet, D.AdoHcy = metmodel(u, p, t)
+    return nothing
 end
