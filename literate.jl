0788828445b789575ead4cfe1382c5d4b2b0448e
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-03-26 08:46:34 +0000

    rm comments

--- literate.jl
+++ literate.jl
@@ -31,16 +31,3 @@ end
 for (nb, t) in zip(nbs, ts)
     println(nb, " elapsed/error: ", t)
 end
-
-# using Literate
-
-# Literate.notebook("docs/hw-01.jl", "docs/"; config)
-# Literate.notebook("docs/intro-01-first-steps.jl", "docs/"; config)
-# Literate.notebook("docs/intro-02-plotting.jl", "docs/"; config)
-# Literate.notebook("docs/intro-03-diffeq.jl", "docs/"; config)
-# Literate.notebook("docs/intro-04-gillespie.jl", "docs/"; config)
-# Literate.notebook("docs/mmsb-1.jl", "docs/"; config)
-# Literate.notebook("docs/mmsb-2.jl", "docs/"; config)
-# Literate.notebook("docs/mmsb-3.jl", "docs/"; config)
-# Literate.notebook("docs/mmsb-4.jl", "docs/"; config)
-# Literate.notebook("docs/mmsb-5.jl", "docs/"; config)
