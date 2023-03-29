a2fab1740a4b36a7d2985400f5df3ee5b086043e
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-03-29 16:57:29 +0800

    debug notebook exec

--- literate.jl
+++ literate.jl
@@ -7,11 +7,21 @@ end
 
 folder = joinpath(@__DIR__, "docs")
 nbs = [nb for nb in readdir(folder) if endswith(nb, ".jl")]
+config = Dict("mdstrings" => true, "execute" => true)
 
 ts = pmap(nbs; on_error=ex->NaN) do nb
-    @elapsed Literate.notebook(joinpath(folder, nb), folder; mdstrings=true)
+    @elapsed Literate.notebook(joinpath(folder, nb), folder; config)
 end
 
 pretty_table([nbs ts], header=["Notebook", "Elapsed (s)"])
+
+for (i, t) in ts
+    if isnan(t)
+        println("Debugging notebook: ", nb[i])
+        withenv("JULIA_DEBUG" => "Literate") do
+            Literate.notebook(joinpath(folder, nb), folder; config)
+        end
+    end
+end
 
 any(isnan, ts) && error("Error(s) occured!")
