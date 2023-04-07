0799e24b600c8859ede76a971b2c9f38e7607567
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-04-07 23:53:49 +0800

    fig 6.07

---
+++ docs/mmsb-6-07.jl
@@ -0,0 +1,15 @@
+#===
+# Fig 6.07
+
+Goldbeter Koshland switch
+===#
+
+using Plots
+Plots.default(linewidth=2)
+
+f607(w, K1, K2) = w * (1 - w + K1)/((1 - w) * (w + K2))
+
+plot(title = "Fig 6.07", xlims=(0, 3), ylims=(0, 1))
+plot!(x -> f607(x, 1, 1), y -> y, 0, 1, label="K1=K2=1")
+plot!(x -> f607(x, 0.1, 0.1), y -> y, 0, 1, label="K1=K2=0.1")
+plot!(x -> f607(x, 0.01, 0.01), y -> y, 0, 1, label="K1=K2=0.01")
