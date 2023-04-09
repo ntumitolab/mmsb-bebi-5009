07595a93bf6a265fccb6e900c267e195e9027957
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-04-09 16:19:56 +0800

    Use forwarddiff

--- docs/mmsb-4-22.jl
+++ docs/mmsb-4-22.jl
@@ -1,16 +1,23 @@
 # # Fig 4.22
 # Tangent line
-import DisplayAs.PNG
+using ForwardDiff
 using Plots
 Plots.default(linewidth=2)
 
+# Curve
+f = x -> 3 / (x-2)
+
+# x = 4
+slope = ForwardDiff.derivative(f, 4)
+yintercept = f(4)
+# Tangent line
+g = x -> slope * (x - 4) + yintercept
+
 fig = plot(title="Fig 4.22 Tangent line")
-plot!(fig, t -> 3 / (t-2), 2.2, 8.0, lab="Curve")
-plot!(fig, t -> 1.5 - (t - 4) * 0.75, 2.7, 5.3, lab="Tangent line")
+plot!(fig, f, 2.2, 8.0, lab="Curve")
+plot!(fig, g, 2.7, 5.3, lab="Tangent line")
 plot!(fig, xlabel="Reaction rate", ylabel="Inhibitor concentration",
       xlims=(2., 8.), ylims=(0., 4.))
-
-fig |> PNG
 
 # ## Runtime information
 
