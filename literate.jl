39bcb0aac81016033262bf42d4909fcf2a1be30c
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-07-01 16:40:18 +0800

    single process

--- literate.jl
+++ literate.jl
@@ -1,43 +1,35 @@
-using Distributed
 using PrettyTables
-
-@everywhere begin
-    using Literate
-end
+using Literate
 
 basedir = "docs/"
 config = Dict("mdstrings" => true, "execute" => true)
 
 nbs = String[]
+ts = Float64[]
 
 # Collect the list of Literate notebooks
 for (root, dirs, files) in walkdir(basedir)
     for file in files
         if (endswith(file, ".jl"))
-            push!(nbs, joinpath(root, file))
+            nb = joinpath(root, file)
+            push!(nbs, nb)
         end
     end
 end
 
-# Execute the notebooks in worker processes
-ts = pmap(nbs; on_error=ex->NaN) do nb
-    @elapsed Literate.notebook(nb, dirname(nb); config)
+# Execute notebooks
+for nb in nbs
+    try
+        t = withenv("JULIA_DEBUG" => "Literate") do
+            @elapsed Literate.notebook(nb, dirname(nb); config)
+        end
+        push!(ts, t)
+    catch e
+        println("An error occured in the cell above:", e)
+        push!(ts, NaN)
+    end
 end
 
 pretty_table([nbs ts], header=["Notebook", "Elapsed (s)"])
-
-# Debug notebooks one by one if there are errors
-for (nb, t) in zip(nbs, ts)
-    if isnan(t)
-        println("Debugging notebook: ", nb)
-        try
-            withenv("JULIA_DEBUG" => "Literate") do
-                Literate.notebook(nb, tempdir(); config)
-            end
-        catch e
-            println("An error occured in the cell above.")
-        end
-    end
-end
 
 any(isnan, ts) && error("Please checkout errors.")
