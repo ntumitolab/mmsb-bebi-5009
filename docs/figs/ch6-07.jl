e41bae77f55c53e08c6aa33cf443acdb0b3f7691
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-10-04 23:48:22 +0800

    CI

--- docs/figs/ch6-07.jl
+++ docs/figs/ch6-07.jl
@@ -6,7 +6,6 @@ Goldbeter Koshland switch
 
 using Plots
 Plots.default(linewidth=2)
-import DisplayAs.SVG
 
 f607(w, K1, K2) = w * (1 - w + K1)/((1 - w) * (w + K2))
 
@@ -14,5 +13,3 @@ fig = plot(title = "Fig 6.07", xlims=(0, 3), ylims=(0, 1))
 plot!(fig, x -> f607(x, 1, 1), y -> y, 0, 1, label="K1=K2=1")
 plot!(fig, x -> f607(x, 0.1, 0.1), y -> y, 0, 1, label="K1=K2=0.1")
 plot!(fig, x -> f607(x, 0.01, 0.01), y -> y, 0, 1, label="K1=K2=0.01")
-
-fig |> SVG
