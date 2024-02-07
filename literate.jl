c11ac8fd4eebcfcc9d9c5967a9635f579e62d727
Wen Wei Tseng <sosiristseng@gmail.com>
2024-02-07 19:57:12 +0800

    cache nb

--- literate.jl
+++ literate.jl
@@ -1,5 +1,6 @@
 using Distributed
 using PrettyTables
+using SHA
 
 @everywhere begin
     ENV["GKSwstype"] = "100"
@@ -13,8 +14,26 @@ nbs = String[]
 # Collect the list of Literate notebooks (ends with .jl)
 for (root, dirs, files) in walkdir(basedir)
     for file in files
-        if (endswith(file, ".jl"))
-            push!(nbs, joinpath(root, file))
+        if endswith(file, ".jl")
+            nb = joinpath(root, file)
+            shaval = read(nb, String) |> sha256 |> bytes2hex
+            @info "$(nb): SHA256=$(shaval)"
+            shafilename = splitext(nb)[1] * ".sha"
+            # Cache hit
+            if isfile(shafilename) && read(shafilename, String) == shaval
+                @info "Notebook $(nb) cache hits and will not be executed."
+            # Cache miss
+            else
+                write(shafilename, shaval)
+                push!(nbs, nb)
+            end
+        # Remove notebook and sha file if the respective literate notebook does not exist
+        elseif endswith(file, ".ipynb") || endswith(file, ".sha")
+            filename = joinpath(root, file)
+            nb = splitext(filename)[1] * ".jl"
+            if !isfile(nb)
+                rm(filename)
+            end
         end
     end
 end
