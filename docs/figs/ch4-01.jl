196122f155aab0f1ca94ab09470f2b4f35e5ab3c
Wen Wei Tseng <sosiristseng@gmail.com>
2024-12-17 20:33:34 +0800

    wip

--- docs/figs/ch4-01.jl
+++ docs/figs/ch4-01.jl
@@ -5,54 +5,47 @@ Steady states and phase plots in an asymmetric network.
 ===#
 using DifferentialEquations
 using SimpleUnPack              ## @unpack
+using ModelingToolkit
 using Plots
 Plots.default(linewidth=2)
 
-# Convenience functions
-hil(x, k) = x / (x + k)
-hil(x, k, n) = hil(x^n, k^n)
+# The model for figure 4.1, 4.2, and 4.3.
+@independent_variables t
+@variables A(t) B(t)
+@parameters k1 k2 k3 k4 k5 n
+D = Differential(t)
 
-# The model and derivatives for figure 4.1, 4.2, and 4.3.
-
-function ∂A41(u, p, t)
-    a, b = u
-    @unpack k1, k2, k3, k4, k5, n = p
-    return da = k1 * hil(1, b, n) - (k3 + k5) * a
-end
-
-function ∂B41(u, p, t)
-    a, b = u
-    @unpack k1, k2, k3, k4, k5, n = p
-    return db = k2 + k5 * a - k4 * b
-end
-
-function model41!(D, u, p, t)
-    D[1] = ∂A41(u, p, t)
-    D[2] = ∂B41(u, p, t)
-    return nothing
-end
+@mtkbuild osys = ODESystem([
+    D(A) ~ k1 / (1 + B^n) - (k3 + k5) * A,
+    D(B) ~ k2 + k5 * A - k4 * B,
+], t)
 
 # ## Fig 4.2 A
+tend = 1.5
+ps1 = Dict(k1=>20, k2=>5, k3=>5, k4=>5, k5=>2, n=>4)
+prob = ODEProblem(osys, [0.0, 0.0], tend, ps1)
 
-ps1 = (k1=20., k2=5., k3=5., k4=5., k5=2., n=4.)
-u0s = (
+u0s = [
     [0.0, 0.0],
     [0.5, 0.6],
     [0.17, 1.1],
     [0.25, 1.9],
     [1.85, 1.7],
-)
-
-tend = 1.5
+]
 
 #---
-sols = [solve(ODEProblem(model41!, u0, tend, ps1)) for u0 in u0s];
+sols = map(u0s) do u0
+    newprob = remake(prob, u0=u0)
+    solve(newprob)
+end
+
+prob.f([0.0, 0.0], prob.p, nothing)
 
 #---
-plot(sols[1], xlabel="Time", ylabel="Concentration", title="Fig. 4.2 A (Time series)", labels=["[A]" "[B]"])
+plot(sols[1], xlabel="Time", ylabel="Concentration", title="Fig. 4.2 A (Time series)")
 
 # ## Fig. 4.2 B (Phase plot)
-plot( sols[1], idxs=(1, 2),
+plot( sols[1], idxs=(A, B),
     xlabel="[A]", ylabel="[B]",
     aspect_ratio=:equal, legend=nothing,
     title="Fig. 4.2 B (Phase plot)",
@@ -74,23 +67,15 @@ plot!(fig, xlabel="Time", ylabel="Concentration")
 fig = plot(title="Fig. 4.3 B (Phase plot)")
 
 for sol in sols
-    plot!(fig, sol, idxs=(1, 2), legend=nothing)
+    plot!(fig, sol, idxs=(A, B), legend=nothing)
 end
 
 plot!(fig, xlabel="[A]", ylabel="[B]", xlims=(0., 2.), ylims=(0., 2.), aspect_ratio=:equal)
 
 # Let's sketch vector fields in phase plots.
-∂A = function (x, y)
-    ∂A41((x, y), ps1, 0.0)
-end
-
-∂B = function (x, y)
-    ∂B41((x, y), ps1, 0.0)
-end
 
 ∂F44 = function (x, y; scale=20)
-    da = ∂A(x, y)
-    db = ∂B(x, y)
+    da, db = prob.f([x, y], prob.p, nothing)
     s = sqrt(hypot(da, db)) * scale
     return (da / s, db / s)
 end
@@ -106,11 +91,12 @@ fig = plot(title="Fig. 4.4 A (Phase plot with vector field)")
 quiver!(fig, xx, yy, quiver=∂F44, line=(:lightgrey), arrow=(:closed), aspect_ratio=:equal)
 
 for sol in sols
-    plot!(fig, sol, idxs=(1, 2), linealpha=0.7, legend=nothing)
+    plot!(fig, sol, idxs=(A, B), linealpha=0.7, legend=nothing)
 end
 
 plot!(fig, size=(600, 600), xlims=(rxy[1], rxy[end]), ylims=(rxy[1], rxy[end]), xlabel="[A]", ylabel="[B]")
 
+# TODO: start from here
 # ## Figure 4.5A
 
 fig = plot(title="Fig. 4.5 A (Phase plot with nullclines)")
