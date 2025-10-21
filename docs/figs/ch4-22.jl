eeca5d023d62c9dccb736dcdb32dc8dbcf835917
sosiristseng <sosiristseng@gmail.com>
2025-10-21 07:22:50 +0000

    self-hosted

--- docs/figs/ch4-22.jl
+++ docs/figs/ch4-22.jl
@@ -2,10 +2,7 @@
 # Fig 4.22
 
 Tangent line.
-
-Using ForwardDiff to calculate the derivative (slope of the tangent) on a curve.
 ===#
-using ForwardDiff
 using Plots
 Plots.default(linewidth=2)
 
@@ -13,15 +10,12 @@ Plots.default(linewidth=2)
 f = x -> 3 / (x-2)
 
 # slope and y intercept for x = 4
-slope = ForwardDiff.derivative(f, 4)
-yintercept = f(4)
+t = x -> -3 / (4 - 2)^2 * (x - 4) + f(4)
 
 #---
-g = x -> slope * (x - 4) + yintercept
-
 fig = plot(title="Fig 4.22")
 plot!(fig, f, 2.2, 8.0, lab="Curve")
-plot!(fig, g, 2.7, 5.3, lab="Tangent line")
+plot!(fig, t, 2.7, 5.3, lab="Tangent line")
 plot!(fig, xlabel="Reaction rate", ylabel="Inhibitor concentration",
       xlims=(2., 8.), ylims=(0., 4.)
 )
