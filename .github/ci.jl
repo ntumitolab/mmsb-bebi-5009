6a23cef5bb49136d4be12ba3d88bfe02603b3fdb
Wen Wei Tseng <sosiristseng@gmail.com>
2025-12-17 21:28:37 +0800

    jbook 2

--- .github/ci.jl
+++ .github/ci.jl
@@ -8,6 +8,8 @@ using SHA
     using Literate, JSON
 end
 
+
+
 # Strip SVG output from a Jupyter notebook
 @everywhere function strip_svg(nbpath)
     oldfilesize = filesize(nbpath)
@@ -20,9 +22,6 @@ end
             if haskey(datadict, "image/png") || haskey(datadict, "image/jpeg")
                 delete!(datadict, "text/html")
                 delete!(datadict, "image/svg+xml")
-            end
-            if haskey(datadict, "text/latex")
-                delete!(datadict, "text/plain")
             end
         end
     end
@@ -130,6 +129,7 @@ function main(;
         ts_lit = pmap(litnbs; on_error=identity) do nb
             @elapsed run_literate(nb, cachedir; rmsvg)
         end
+        rmprocs(workers()) # Remove worker processes to release some memory
         failed = false
         for (nb, t) in zip(litnbs, ts_lit)
             if t isa ErrorException
