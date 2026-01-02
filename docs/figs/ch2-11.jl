0962003cd8969f22b2314c355b3ef18147f81838
Wen Wei Tseng <sosiristseng@gmail.com>
2026-01-02 22:51:41 +0800

    WIP

--- docs/figs/ch2-11.jl
+++ docs/figs/ch2-11.jl
@@ -51,9 +51,9 @@ plot(
 
 # ## Figure 2.12
 # Rapid equilibrium assumption
-_a212(u, p, t) =  u.C * p.k1 / (p.km1 + p.k1)
+_a212(u, p, t) =  u.C * p.km1 / (p.km1 + p.k1)
 function model212!(du, u, p, t)
-    @unpack k0, k1, km1, k2 = p
+    @unpack k0, k2 = p
     @unpack C = u
     A = _a212(u, p, t)
     B = C - A
@@ -64,10 +64,8 @@ function model212!(du, u, p, t)
 end
 
 #---
-ps212 = ps211
-u0212 = ComponentArray(C = sum(u0))
 tend = 3.0
-prob212 = ODEProblem(model212!, u0212, tend, ps212)
+prob212 = ODEProblem(model212!, ComponentArray(C = sum(u0)), tend, ps211)
 
 #---
 @time sol212 = solve(prob212)
@@ -89,20 +87,28 @@ plot!(fig,
 fig
 
 #===
-## Figure 2.13: Rapid equilibrium (take 2)
+## Figure 2.13
+
+Rapid equilibrium (take 2)
 
 When another set of parameters is not suitable for rapid equilibrium assumption.
 ===#
 
-ps2 = [k0 => 9.0, k1 => 20.0, km1 => 12.0, k2 => 2.0]
-u0 = [A => 8.0, B => 4.0]
+ps213 =ComponentArray(k0 = 9.0, k1 = 20.0, km1 = 12.0, k2 = 2.0)
+u0 = ComponentArray(A = 8.0, B = 4.0)
 tend = 3.0
 
-sol213full = solve(ODEProblem(rn211, u0, tend, ps2))
-sol213re = solve(ODEProblem(model212, [C => sum(last.(u0))], tend, ps2))
+@time sol213full = solve(ODEProblem(model211!, u0, tend, ps213))
+@time sol213re = solve(ODEProblem(model212!, ComponentArray(C = sum(u0)), tend, ps213))
+
+#---
 
 fig = plot(sol213full, line=(:dash, 1), label=["A (full solution)" "B (full solution)"])
-plot!(fig, sol213re, idxs=[A, B], label=["A (rapid equilibrium)" "B (rapid equilibrium)"])
+ts = sol213re.t
+cs = sol213re[1, :]
+as = _a212.(sol213re.u, Ref(ps213), ts)
+bs = cs .- as
+plot!(fig, ts, [as bs], label=["A (rapid equilibrium)" "B (rapid equilibrium)"])
 plot!(fig,
     title="Fig. 2.13 (Rapid equilibrium model)",
     xlabel="Time (AU)",
@@ -116,27 +122,27 @@ fig
 
 Quasi-steady state assumption on species A
 ===#
-
-function make_214(; name)
-    @parameters k0 k1 km1 k2
-    @independent_variables t
-    @variables A(t) B(t)
-    D = Differential(t)
-    eqs = [
-        A ~ (k0 + km1 * B) / k1
-        D(B) ~ k1 * A - (km1 + k2) * B
-    ]
-    return ODESystem(eqs, t; name)
+_a214(u, p, t) = (p.k0 + p.km1 * u.B) / p.k1
+_u0214(u0, p) = (p.k1 * u0 - p.k0) / (p.k1 + p.km1)
+function model214!(du, u, p, t)
+    @unpack k0, k1, km1, k2 = p
+    @unpack B = u
+    A = _a214(u, p, t)
+    du.B = k1 * A - (km1 + k2) * B
+    nothing
 end
 
-#---
-@mtkbuild model214 = make_214()
+ps214 = ps213
+u0214= ComponentArray(B = _u0214(sum(u0), ps214))
 
-# Initial conditions can also be represented in symbols
-sol214 = solve(ODEProblem(model214, [B => (k1 * sum(last.(u0)) - k0) / (k1 + km1)], tend, ps2))
+# Solve QSSA model
+@time sol214 = solve(ODEProblem(model214!, u0214, tend, ps214))
 
-fig = plot(sol213full, line=(:dash))
-plot!(fig, sol214, idxs=[A, B], label=["A (QSSA)" "B (QSSA)"])
+fig = plot(sol213full, line=(:dash), label=["A (full solution)" "B (full solution)"])
+ts = sol214.t
+as = _a214.(sol214.u, Ref(ps214), ts)
+bs = sol214[1, :]
+plot!(fig, ts, [as bs], label=["A (QSSA)" "B (QSSA)"])
 plot!(fig,
     xlabel="Time (AU)",
     ylabel="Concentration (AU)",
