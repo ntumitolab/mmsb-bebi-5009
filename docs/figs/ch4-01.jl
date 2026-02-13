3292135f6a8c20eb574b073343c55aa9742b9a2e
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-13 17:16:17 +0800

    wip

--- docs/figs/ch4-01.jl
+++ docs/figs/ch4-01.jl
@@ -7,16 +7,15 @@ using OrdinaryDiffEq
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using Plots
-Plots.gr(framestyle = :box)
-import PythonPlot as plt
+Plots.gr(framestyle = :box, linewidth=1.5)
 
 #---
 function model401(; tend = 1.5)
     @parameters k1=20 k2=5 k3=5 k4=5 k5=2 n=4
-    @variables A(t)=0.0 B(t)=0.0
+    @variables s1(t)=0.0 s2(t)=0.0
     eqs = [
-        D(A) ~ k1 / (1 + B^n) - (k3 + k5) * A
-        D(B) ~ k2 + k5 * A - k4 * B
+        D(s1) ~ k1 / (1 + s2^n) - (k3 + k5) * s1
+        D(s2) ~ k2 + k5 * s1 - k4 * s2
     ]
     @mtkcompile sys = ODESystem(eqs, t)
     return ODEProblem(sys, [], tend)
@@ -25,14 +24,14 @@ end
 # ## Fig 4.2 A
 @time prob402a = model401()
 
-@unpack A, B = prob402a.f.sys
+@unpack s1, s2 = prob402a.f.sys
 
 u0s = [
-    [A => 0.0, B => 0.0],
-    [A => 0.5, B => 0.6],
-    [A => 0.17, B => 1.1],
-    [A => 0.25, B => 1.9],
-    [A => 1.85, B => 1.7]
+    [s1 => 0.0, s2 => 0.0],
+    [s1 => 0.5, s2 => 0.6],
+    [s1 => 0.17, s2 => 1.1],
+    [s1 => 0.25, s2 => 1.9],
+    [s1 => 1.85, s2 => 1.7]
 ]
 
 #---
@@ -42,10 +41,10 @@ end
 
 #---
 plot(sols[1], xlabel="Time", ylabel="Concentration", title="Fig. 4.2 A")
-fig
+
 
 # ## Fig. 4.2 B (Phase plot)
-plot(sols[1], idxs=(A, B), xlabel="[A]", ylabel="[B]", title="Fig. 4.2 B", aspect_ratio=1, size=(600, 600), xlims=(0, 2), ylims=(0, 2), legend=nothing)
+plot(sols[1], idxs=(s1, s2), xlabel="[S1]", ylabel="[S2]", title="Fig. 4.2 B", aspect_ratio=1, size=(600, 600), xlims=(0, 2), ylims=(0, 2), legend=nothing)
 
 
 # ## Fig. 4.3 A (Multiple time series)
@@ -58,37 +57,42 @@ end
 pl43
 
 # ## Fig. 4.3 B (Phase plot)
-pl43B = plot(xlabel="[A]", ylabel="[B]", title = "Fig. 4.3 B", aspect_ratio=1, size=(600, 600), xlims=(0, 2), ylims=(0, 2), legend=nothing)
+pl43B = plot(xlabel="[S1]", ylabel="[S2]", title = "Fig. 4.3 B", aspect_ratio=1, size=(600, 600), xlims=(0, 2), ylims=(0, 2), legend=nothing)
 
 for sol in sols
-    plot!(pl43B, sol, idxs=(A, B), label=nothing)
+    plot!(pl43B, sol, idxs=(s1, s2), label=nothing)
 end
 
 pl43B
 # ## Fig. 4.4 A
 # Vector fields in phase plots
-∂F44 = prob402a.f
-ps402a = prob402a.p
-xx = collect(0:0.1:2)
-yy = collect(0:0.1:2)
-## X and Y velocities are reversed for streamplot in matplotlib
-U = [∂F44([x, y], ps402a, nothing)[2] for x in xx, y in yy]
-V = [∂F44([x, y], ps402a, nothing)[1] for x in xx, y in yy]
+function plot_gradient(prob, xsym, ysym; maxlength=1, xmin=0, ymin=0, xmax=2, ymax=2, xstep=0.1, ystep=0.1, t = nothing, aspect_ratio=1, size=(600, 600), kwargs...)
+    swap_or_not(x, y; xidx=1) = xidx == 1 ? [x, y] : [y, x]
 
-plt.figure(figsize=(6, 6))
-plt.quiver(xx, yy, U, V, hypot.(U, V))
-for sol in sols
-    ts = 0:0.01:1.5
-    aa = sol(ts, idxs=A)
-    bb = sol(ts, idxs=B)
-    plt.plot(aa, bb, color="black")
+    ∂F = prob.f
+    ps = prob.p
+    sys = prob.f.sys
+    xidx = ModelingToolkit.variable_index(sys, xsym)
+    yidx = ModelingToolkit.variable_index(sys, ysym)
+    ## Raw vector field
+    xx = [x for y in ymin:ystep:ymax, x in xmin:xstep:xmax]
+    yy = [y for y in ymin:ystep:ymax, x in xmin:xstep:xmax]
+    dx = map((x, y) -> ∂F(swap_or_not(x, y; xidx), ps, t)[xidx], xx, yy)
+    dy = map((x, y) -> ∂F(swap_or_not(x, y; xidx), ps, t)[yidx], xx, yy)
+    ## Normalize vector field
+    maxnorm = maximum(hypot.(dx, dy))
+    @. dx = dx / maxnorm * maxlength
+    @. dy = dy / maxnorm * maxlength
+    quiver(xx, yy, quiver=(dx, dy); aspect_ratio=aspect_ratio, size=size, xlims=(xmin, xmax), ylims=(ymin, ymax), kwargs...)
 end
-plt.xlabel("[A]")
-plt.ylabel("[B]")
-plt.title("Fig. 4.4 A\nVector field in phase plot")
-plt.xlim(0, 2)
-plt.ylim(0, 2)
-plt.gcf()
+
+pl44a = plot_gradient(prob402a, s1, s2; maxlength=0.1, color=:lightblue)
+
+for sol in sols
+    plot!(pl44a, sol, idxs=(s1, s2), color=:black, label=nothing)
+end
+
+plot!(pl44a, title="Fig. 4.4 A", xlabel="[S1]", ylabel="[S2]")
 
 
 # ## Figure 4.5A
