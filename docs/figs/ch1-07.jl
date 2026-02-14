0f5078b5a5f8b5905141c22c3ff4fe5c014636c1
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-14 16:37:52 +0800

    1-07

--- docs/figs/ch1-07.jl
+++ docs/figs/ch1-07.jl
@@ -1,8 +1,5 @@
-#===
-# Fig 1.7
-
-For Figures 1.7, 7.13, 7.14, 7.15.
-===#
+# # Fig 1.7
+# For Figures 1.7, 7.13, 7.14, 7.15.
 using OrdinaryDiffEq
 using DiffEqCallbacks
 using ComponentArrays: ComponentArray
@@ -26,28 +23,24 @@ ps = ComponentArray(a1=3.0, a2=2.5, β=4.0, γ=4.0, i1=0.0, i2=0.0)
 u0 = ComponentArray(s1=0.075, s2=2.5)
 prob = ODEProblem(collins!, u0, (0.0, tend), ps)
 
-# Callbacks and solve the problem
-affect_i2_on!(integrator) = integrator.p.i2 = 10.0
-affect_i2_off!(integrator) = integrator.p.i2 = 0.0
-affect_i1_on!(integrator) = integrator.p.i1 = 10.0
-affect_i1_off!(integrator) = integrator.p.i1 = 0.0
-cb_i2_on = PresetTimeCallback(10.0, affect_i2_on!)
-cb_i2_off = PresetTimeCallback(20.0, affect_i2_off!)
-cb_i1_on = PresetTimeCallback(30.0, affect_i1_on!)
-cb_i1_off = PresetTimeCallback(40.0, affect_i1_off!)
-cbs = CallbackSet(cb_i2_on, cb_i2_off, cb_i1_on, cb_i1_off)
+#--- Callbacks and solve the problem
+cbs = let
+    affect_i2_on!(integrator) = integrator.p.i2 = 10.0
+    affect_i2_off!(integrator) = integrator.p.i2 = 0.0
+    affect_i1_on!(integrator) = integrator.p.i1 = 10.0
+    affect_i1_off!(integrator) = integrator.p.i1 = 0.0
+    cb_i2_on = PresetTimeCallback(10.0, affect_i2_on!)
+    cb_i2_off = PresetTimeCallback(20.0, affect_i2_off!)
+    cb_i1_on = PresetTimeCallback(30.0, affect_i1_on!)
+    cb_i1_off = PresetTimeCallback(40.0, affect_i1_off!)
+    cbs = CallbackSet(cb_i2_on, cb_i2_off, cb_i1_on, cb_i1_off)
+end
 
 @time sol = solve(prob, Tsit5(), callback=cbs)
 
-# Visualization
-fig = Figure()
-ax = Axis(
-    fig[1, 1],
-    xlabel="Time",
-    ylabel="Concentration",
-    title="Fig. 1.7 (Collins Toggle Switch)"
-)
-lines!(ax, 0 .. tend, t -> sol(t).s1, label="s1")
-lines!(ax, 0 .. tend, t -> sol(t).s2, label="s2")
-axislegend(ax, position=:rt)
+#--- Visualization
+ts = 0:0.1:tend
+data = Array(sol(ts; idxs=[1, 2]))
+fig, ax, sp = series(ts, data; labels=["s1", "s2"], axis=(title="Fig. 1.7", xlabel="Time", ylabel="Concentration"))
+axislegend(ax)
 fig
