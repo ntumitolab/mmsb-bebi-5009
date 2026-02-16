651d2475f24ab6dded38d49cdc0954991fbe695e
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-16 22:19:27 +0800

    ch4

--- docs/figs/ch4-15.jl
+++ docs/figs/ch4-15.jl
@@ -6,28 +6,25 @@ using ComponentArrays: ComponentArray
 using SimpleUnPack
 using CairoMakie
 
-# The model
-_dA415(A, B, p, t) = p.k0 - p.k1 * A * (1 + B^p.n)
-_dB415(A, B, p, t) = p.k1 * A * (1 + B^p.n) - p.k2 * B
-function model415!(D, u, p, t)
+#---
+function model415(u, p, t)
     @unpack A, B = u
-    D.A = _dA415(A, B, p, t)
-    D.B = _dB415(A, B, p, t)
+    @unpack k0, k1, k2, n = p
+    v1 = k1 * A * (1 + B^n)
+    dA = k0 - v1
+    dB = v1 - k2 * B
+    return (; dA, dB)
+end
+function model415!(D, u, p, t)
+    @unpack dA, dB = model415(u, p, t)
+    D.A = dA
+    D.B = dB
     nothing
 end
 
 #---
-ps415 = ComponentArray(
-    k0 = 8.0,
-    k1 = 1.0,
-    k2 = 5.0,
-    n = 2.0
-)
-u0415 = ComponentArray(
-    A = 1.5,
-    B = 1.0
-)
-
+ps415 = ComponentArray( k0 = 8.0, k1 = 1.0, k2 = 5.0, n = 2.0)
+u0415 = ComponentArray( A = 1.5, B = 1.0)
 tend = 8.0
 prob415 = ODEProblem(model415!, u0415, (0.0, tend), ps415)
 
@@ -56,18 +53,11 @@ fig
 
 # ## Fig 4.15 (B)
 ∂F415  = function (x, y)
-    da = _dA415(x, y, ps415, nothing)
-    db = _dB415(x, y, ps415, nothing)
-    return Point2d(da, db)
+    @unpack dA, dB = model415((; A=x, B=y), ps415, 0.0)
+    return Point2d(dA, dB)
 end
 
-## Grid points
-xx = 0:0.01:4
-yy = 0:0.01:4
-ts = 0:0.05:tend
-∂A415 = [_dA415(x, y, ps415, nothing) for x in xx, y in yy]
-∂B415 = [_dB415(x, y, ps415, nothing) for x in xx, y in yy]
-
+## Stream plot
 fig = Figure(size=(600, 600))
 ax = Axis(fig[1, 1],
     xlabel = "[A]",
@@ -75,14 +65,22 @@ ax = Axis(fig[1, 1],
     title = "Fig. 4.15 B\nPhase plot",
     aspect = 1,
 )
-streamplot!(ax, ∂F415, xx, yy)
+streamplot!(ax, ∂F415, 0..4, 0..4)
 
+## Trajectories
 for sol in sols
+    ts = 0:0.05:tend
     aa = [sol(t).A for t in ts]
     bb = [sol(t).B for t in ts]
-    lines!(ax, aa, bb, color=:tomato)
+    lines!(ax, aa, bb)
 end
 limits!(ax, 0.0, 4.0, 0.0, 4.0)
+
+## Nullclines
+xx = 0:0.01:4
+yy = 0:0.01:4
+∂A415 = [model415((; A=x, B=y), ps415, nothing).dA for x in xx, y in yy]
+∂B415 = [model415((; A=x, B=y), ps415, nothing).dB for x in xx, y in yy]
 contour!(ax, xx, yy, ∂A415, levels=[0], color=:black, linestyle=:solid, linewidth=2, label="A nullcline")
 contour!(ax, xx, yy, ∂B415, levels=[0], color=:black, linestyle=:dash, linewidth=2, label="B nullcline")
 axislegend(ax, position = :rb)
@@ -93,13 +91,7 @@ fig
 
 Oscillatory parameter set
 ===#
-ps416 = ComponentArray(
-    k0 = 8.0,
-    k1 = 1.0,
-    k2 = 5.0,
-    n = 2.5
-)
-
+ps416 = ComponentArray( k0 = 8.0, k1 = 1.0, k2 = 5.0, n = 2.5)
 tend = 100.0
 u0s = [
     ComponentArray(A=1.5, B=1.0),
@@ -109,7 +101,6 @@ u0s = [
 ]
 
 prob416 = ODEProblem(model415!, u0415, tend, ps416)
-
 @time sols = map(u0s) do u0
     solve(remake(prob416, u0=u0), Tsit5())
 end
@@ -127,16 +118,9 @@ fig
 
 # ## Fig 4.16 b
 ∂F416 = function (x, y)
-    da = _dA415(x, y, ps416, nothing)
-    db = _dB415(x, y, ps416, nothing)
-    return Point2d(da, db)
+    @unpack dA, dB = model415((; A=x, B=y), ps416, nothing)
+    return Point2d(dA, dB)
 end
-
-xx = 0:0.01:4
-yy = 0:0.01:4
-
-∂A416 = [_dA415(x, y, ps416, nothing) for x in xx, y in yy]
-∂B416 = [_dB415(x, y, ps416, nothing) for x in xx, y in yy];
 
 fig = Figure(size=(600, 600))
 ax = Axis(fig[1, 1],
@@ -146,10 +130,21 @@ ax = Axis(fig[1, 1],
     aspect = 1,
 )
 
-streamplot!(ax, ∂F416, xx, yy)
+## Stream plot
+streamplot!(ax, ∂F416, 0..4, 0..4)
+
+## Nullclines
+xx = 0:0.01:4
+yy = 0:0.01:4
+∂A416 = [model415((; A=x, B=y), ps416, nothing).dA for x in xx, y in yy]
+∂B416 = [model415((; A=x, B=y), ps416, nothing).dB for x in xx, y in yy]
+
 contour!(ax, xx, yy, ∂A416, levels=[0], color=:black, linestyle=:solid, linewidth=2, label="A nullcline")
 contour!(ax, xx, yy, ∂B416, levels=[0], color=:black, linestyle=:dash, linewidth=2, label="B nullcline")
+
+## Trajectories
 for sol in sols
+    ts = 0:0.01:tend
     aa = [sol(t).A for t in ts]
     bb = [sol(t).B for t in ts]
     lines!(ax, aa, bb, color=:tomato)
@@ -173,8 +168,8 @@ ax = Axis(fig[1, 1],
 lines!(ax, sol, idxs=(1, 2), color=:tomato, label="Trajectory")
 xx = 1:0.01:3
 yy = 1:0.01:3
-∂A417 = [_dA415(x, y, ps416, nothing) for x in xx, y in yy]
-∂B417 = [_dB415(x, y, ps416, nothing) for x in xx, y in yy];
+∂A417 = [model415((; A=x, B=y), ps416, nothing).dA for x in xx, y in yy]
+∂B417 = [model415((; A=x, B=y), ps416, nothing).dB for x in xx, y in yy];
 
 streamplot!(ax, ∂F416, xx, yy)
 contour!(ax, xx, yy, ∂A417, levels=[0], color=:black, linestyle=:solid, linewidth=2, label="A nullcline")
